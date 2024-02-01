40153c80f548db2dd6ac59b7bab236d51f8886c9
oussama <znagui.info@gmail.com>
2024-02-01 20:57:12 +0100

    db est prete

--- project/resources/views/addrecit.blade.php
+++ project/resources/views/addrecit.blade.php
@@ -9,8 +9,8 @@
 </head>
 
 <body>
-    <section class="py-5">
-        <nav class="bg-white border-gray-200 dark:bg-gray-900 px-10">
+    <section class="py-5 bg-gradient-to-tl from-rose-100 to-teal-100">
+        <nav class=" border-gray-200 dark:bg-gray-900 px-10">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img class="h-16" src="{{ asset('images/logo.png') }}" alt="Logo">
@@ -24,22 +24,27 @@
             </div>
         </nav>
 
-        <div class="flex justify-center items-center  h-auto bg-white">
+        <div class="flex justify-center items-center  h-auto ">
 
             <div class="container mx-auto my-4 px-4 lg:px-20">
 
-                <div class="w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
+                <form class="bg-gray-100 w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
                     <div class="flex">
                         <h1 class="font-bold uppercase text-5xl">Ajouter un recit</h1>
                     </div>
-                    <div class="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
-                        <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" placeholder="First Name*" />
-                        <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" placeholder="Last Name*" />
-                        <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="email" placeholder="Email*" />
-                        <input class="w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="number" placeholder="Phone*" />
+                    <div class="grid grid-cols-1 gap-5 md:grid-cols-1 mt-5">
+                        <input class="border-2  w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="file" />
+
+                    </div>
+                    <div class="grid grid-cols-1 gap-5 md:grid-cols-1 mt-5">
+                        <input class="border-2  w-full bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline" type="text" placeholder="Titre*" />
+
                     </div>
                     <div class="my-4">
-                        <textarea placeholder="Message*" class="w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+                        <textarea placeholder="Description*" class="border-2  w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+                    </div>
+                    <div class="my-4">
+                        <textarea placeholder="Conseil" class="border-2  w-full h-32 bg-gray-100 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
                     </div>
                     <div class="my-2 w-1/2 lg:w-1/4">
                         <button class="uppercase text-sm font-bold tracking-wide bg-blue-900 text-gray-100 p-3 rounded-lg w-full 
@@ -47,7 +52,7 @@
                             Send Message
                         </button>
                     </div>
-                </div>
+                </form>
 
                 <div class="w-full lg:-mt-96 lg:w-2/6 px-8 py-12 ml-auto bg-blue-900 rounded-2xl">
                     <div class="flex flex-col text-white">
@@ -57,39 +62,27 @@
                             eu feugiat felis fermentum id. Curabitur vitae nibh viverra, auctor turpis sed, scelerisque ex.
                         </p>
 
-                        <div class="flex my-4 w-2/3 lg:w-1/2">
+                        <div class="flex my-4 w-2/3 ">
+
                             <div class="flex flex-col">
-                                <i class="fas fa-map-marker-alt pt-2 pr-2" />
-                            </div>
-                            <div class="flex flex-col">
-                                <h2 class="text-2xl">Main Office</h2>
-                                <p class="text-gray-400">5555 Tailwind RD, Pleasant Grove, UT 73533</p>
+                                <h2 class="text-2xl">Adresse</h2>
+                                <p class="text-gray-400">N° 123 Bloc F9 Cité Dakhla 80000,Agadir</p>
                             </div>
                         </div>
 
-                        <div class="flex my-4 w-2/3 lg:w-1/2">
+                        <div class="flex my-4  ">
+
                             <div class="flex flex-col">
-                                <i class="fas fa-phone-alt pt-2 pr-2" />
-                            </div>
-                            <div class="flex flex-col">
-                                <h2 class="text-2xl">Call Us</h2>
-                                <p class="text-gray-400">Tel: xxx-xxx-xxx</p>
-                                <p class="text-gray-400">Fax: xxx-xxx-xxx</p>
+                                <h2 class="text-2xl">Appelez-nous</h2>
+                                <p class="text-gray-400">Tel: +212 (0) 669 66 62 05</p>
+                                <p class="text-gray-400">Fax: +212 (0) 528 66 62 05</p>
                             </div>
                         </div>
 
-                        <div class="flex my-4 w-2/3 lg:w-1/2">
-                            <a href="https://www.facebook.com/ENLIGHTENEERING/" target="_blank" rel="noreferrer" class="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1">
-                                <i class="fab fa-facebook-f text-blue-900" />
-                            </a>
-                            <a href="https://www.linkedin.com/company/enlighteneering-inc-" target="_blank" rel="noreferrer" class="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1">
-                                <i class="fab fa-linkedin-in text-blue-900" />
-                            </a>
-                        </div>
+
                     </div>
                 </div>
             </div>
-            <!-- COMPONENT CODE -->
         </div>
 
 
